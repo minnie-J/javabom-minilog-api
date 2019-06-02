5496d5f4d2535a2e89c30c50243eb6f5a06df7db
minnie-J <minhee.jeong@brique.co.kr>
2019-06-02 21:28:32 +0900

    Article Model 생성

--- src/main/java/com/javabom/minilog/domain/Article.java
+++ src/main/java/com/javabom/minilog/domain/Article.java
@@ -1,4 +1,155 @@
 package com.javabom.minilog.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity // (=Model)
+@Table(name="articles") // table name
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment DB에 위임
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ano")
+    private int ano;
+
+    private String title;
+    private String content;
+    private String symbol;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date regdate;
+
+    @Column(name = "class")
+    private String category;
+
+    private String subclass;
+    private double stars;
+    private String reldate;
+    private String relplace;
+    private String relperson;
+    private String tags;
+    private String author;
+
+    public Article() {}
+
+    public Article(int ano, String title, String content, String symbol, Date regdate, String category, String subclass, double stars, String reldate, String relplace, String relperson, String tags, String author) {
+        this.ano = ano;
+        this.title = title;
+        this.content = content;
+        this.symbol = symbol;
+        this.regdate = regdate;
+        this.category = category;
+        this.subclass = subclass;
+        this.stars = stars;
+        this.reldate = reldate;
+        this.relplace = relplace;
+        this.relperson = relperson;
+        this.tags = tags;
+        this.author = author;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubclass() {
+        return subclass;
+    }
+
+    public void setSubclass(String subclass) {
+        this.subclass = subclass;
+    }
+
+    public double getStars() {
+        return stars;
+    }
+
+    public void setStars(double stars) {
+        this.stars = stars;
+    }
+
+    public String getReldate() {
+        return reldate;
+    }
+
+    public void setReldate(String reldate) {
+        this.reldate = reldate;
+    }
+
+    public String getRelplace() {
+        return relplace;
+    }
+
+    public void setRelplace(String relplace) {
+        this.relplace = relplace;
+    }
+
+    public String getRelperson() {
+        return relperson;
+    }
+
+    public void setRelperson(String relperson) {
+        this.relperson = relperson;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
