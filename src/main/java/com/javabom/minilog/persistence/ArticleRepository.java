9ef373a90ac252a24aca7fd0933c59c63e4c1124
minnie-J <minhee.jeong@brique.co.kr>
2019-06-16 02:42:07 +0900

    select 조건 추가 - category

--- src/main/java/com/javabom/minilog/persistence/ArticleRepository.java
+++ src/main/java/com/javabom/minilog/persistence/ArticleRepository.java
@@ -2,7 +2,11 @@ package com.javabom.minilog.persistence;
 
 import com.javabom.minilog.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-
+    // 상속만으로 save(),  findOne(), findAll(), count(), delete()의 기본 기능 자동 제공
+    // 기본 기능 이외의 조회기능을 추가할 때 메소드 추가
+    Article findByCategory(String category);
 }
