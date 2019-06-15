df1d35d34a63c5184add933efbf5d451db027448
minnie-J <minhee.jeong@brique.co.kr>
2019-06-16 03:04:56 +0900

    ArticleService.java 생성

--- src/main/java/com/javabom/minilog/service/ArticleService.java
+++ src/main/java/com/javabom/minilog/service/ArticleService.java
@@ -1,4 +1,12 @@
 package com.javabom.minilog.service;
 
+import com.javabom.minilog.domain.Article;
+
+import java.util.List;
+
 public interface ArticleService {
+    Article findByAuthor(String author);
+    Article findByAuthorAndCategory(String author, String category);
+    Article save(Article article);
+    Article delete(int ano);
 }
