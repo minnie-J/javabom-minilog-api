a136b750ccac08bd9bc9d82ccf169f66297f139e
MinHee Jeong <38336567+minnie-J@users.noreply.github.com>
2019-06-15 23:05:00 +0900

    [domain #13] Member (#14)

---
+++ src/main/java/com/javabom/minilog/domain/Member.java
@@ -0,0 +1,75 @@
+package com.javabom.minilog.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "members")
+public class Member {
+    private String id;
+    private String name;
+    private String pwd;
+    private String email;
+    private String profile;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int mno;
+
+    public Member() {}
+
+    public Member(String id, String name, String pwd, String email, String profile, int mno) {
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+        this.email = email;
+        this.profile = profile;
+        this.mno = mno;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public int getMno() {
+        return mno;
+    }
+
+}
+
+
