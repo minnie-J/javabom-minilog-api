f840f123b0ba9555f10ebebf1a01a5c29c3c5c46
minnie-J <minhee.jeong@brique.co.kr>
2019-06-16 10:27:34 +0900

    repository 설명 추가

--- src/main/java/com/javabom/minilog/persistence/ArticleRepository.java
+++ src/main/java/com/javabom/minilog/persistence/ArticleRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
     // 상속만으로 save(),  findOne(), findAll(), count(), delete()의 기본 기능 자동 제공
-    // 기본 기능 이외의 조회기능을 추가할 때 메소드 추가
-    Article findByCategory(String category);
+    // 기본 기능 이외의 조회 기능을 추가할 때 메소드 추가
+//    Article findByAuthor(String author);
+//    Article findByAuthorAndCategory(String author, String category);
+    Article findAllByCategory(String category);
 }
