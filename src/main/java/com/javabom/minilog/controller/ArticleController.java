6821891ce484436763969e25a1a874ea2f4721a1
MinHee Jeong <38336567+minnie-J@users.noreply.github.com>
2019-06-21 01:11:33 +0900

    [Article #23] article 전체 목록 API (#24)

--- src/main/java/com/javabom/minilog/controller/ArticleController.java
+++ src/main/java/com/javabom/minilog/controller/ArticleController.java
@@ -1,13 +1,15 @@
 package com.javabom.minilog.controller;
 
 import com.javabom.minilog.domain.Article;
+import com.javabom.minilog.domain.JSONResponse;
+import com.javabom.minilog.domain.ResponseWrapper;
 import com.javabom.minilog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class ArticleController {
@@ -17,9 +19,27 @@ public class ArticleController {
 
     @GetMapping(value = "/api/v1/articles")
     // @GetMapping ==> @RequestMapping(method = RequestMethod.GET)의 축약
-    public ResponseEntity<List<Article>> getAllArticles() {
+    public ResponseEntity<ResponseWrapper> getAllArticles() {
         List<Article> articles = articleService.findAll();
-        ResponseEntity<List<Article>> articlesEntity = new ResponseEntity<List<Article>>(articles, HttpStatus.OK);
+
+        List<JSONResponse> jsonResponses = new ArrayList<JSONResponse>();
+        for (Article article : articles) {
+            JSONResponse jsonResponse = new JSONResponse();
+            Map<Object, Object> attribute = new HashMap<Object, Object>();
+            Map<Object, Object> links = new HashMap<Object, Object>();
+            jsonResponse.setType("articles");
+                jsonResponse.setId(article.getId());
+                attribute.put("title", article.getTitle());
+                attribute.put("content", article.getContent());
+                jsonResponse.setAttributes(attribute);
+                links.put("self", "/api/vi/article/" + article.getId());
+                jsonResponse.setLinks(links);
+
+                jsonResponses.add(jsonResponse);
+            }
+
+        ResponseWrapper responseWrapper = new ResponseWrapper(jsonResponses);
+        ResponseEntity<ResponseWrapper> articlesEntity = new ResponseEntity<ResponseWrapper>(responseWrapper, HttpStatus.OK);
         return articlesEntity;
     }
 
