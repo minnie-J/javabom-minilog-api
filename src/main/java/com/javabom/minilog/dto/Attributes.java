7d3997ce1bbb5916e99f5752ca5cb127710681b7
MinHee Jeong <38336567+minnie-J@users.noreply.github.com>
2019-07-02 22:25:43 +0900

    [Article #29] DTO 패키지 생성 (#30)

---
+++ src/main/java/com/javabom/minilog/dto/Attributes.java
@@ -0,0 +1,30 @@
+package com.javabom.minilog.dto;
+
+public class Attributes {
+
+    private String title;
+    private String content;
+
+    public Attributes() {}
+
+    public Attributes(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
