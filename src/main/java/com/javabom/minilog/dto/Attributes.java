a12121573cfd0b3216fdecd3ea7e235d3e3076ad
minnie-J <minhee.jeong@brique.co.kr>
2019-07-05 00:20:47 +0900

    날짜 타입 변경

--- src/main/java/com/javabom/minilog/dto/Attributes.java
+++ src/main/java/com/javabom/minilog/dto/Attributes.java
@@ -1,15 +1,23 @@
 package com.javabom.minilog.dto;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Attributes {
 
     private String title;
     private String content;
+//    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime regdate;
 
     public Attributes() {}
 
-    public Attributes(String title, String content) {
+    public Attributes(String title, String content, LocalDateTime regdate) {
         this.title = title;
         this.content = content;
+        this.regdate = regdate;
     }
 
     public String getTitle() {
@@ -26,5 +34,13 @@ public class Attributes {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public LocalDateTime getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(LocalDateTime regdate) {
+        this.regdate = regdate;
     }
 }
